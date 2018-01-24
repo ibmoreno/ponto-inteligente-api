acaf57ed274fe99305ea4c51bcaac5305a510ddc
ibmoreno <ibmoreno.cba@gmail.com>
2018-01-24 09:42:52 -0300

    Commit inicial dos arquivos do projeto

---
+++ src/main/java/com/kazale/pontointeligente/api/ProntoInteligenteApplication.java
@@ -0,0 +1,12 @@
+package com.kazale.pontointeligente.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProntoInteligenteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProntoInteligenteApplication.class, args);
+	}
+}
